6cc5125fb3261c120ce4587fd3c0d9ad8e41c104
Alejandro Ramirez <josealejandroramz@gmail.com>
2022-03-28 17:24:01 -0400

    Feat agregar componentes de procesos

--- src/app/modules/configuration/sub-modules/nomina/procesos/components/data-table/data-table.component.ts
+++ src/app/modules/configuration/sub-modules/nomina/procesos/components/data-table/data-table.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Procesos } from '../../interfaces/procesos.interfaces';
+import { TableHead } from '../../../../../../../shared/interfaces/tableHead.interfaces';
+import { ProcesosService } from '../../services/procesos.service';
 
 @Component({
   selector: 'app-data-table',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataTableComponent implements OnInit {
 
-  constructor() { }
+  @Input() procesos!: Procesos[];
+
+  // Table
+  columns: TableHead[] = [];
+
+  constructor(private procesosService: ProcesosService) { }
 
   ngOnInit(): void {
+    this.columns = [
+      { field: 'codpro',  header: 'Código' },
+      { field: 'despro',  header: 'Descripción' },
+      { field: 'desadic', header: 'Descripción adicional' },
+      { field: 'codsec',  header: 'Código subproceso' },
+      { field: 'dessec',  header: 'Descripción subproceso' }
+    ];
+  }
+
+  onRowSelect(event: any): void {
+    this.procesosService.selectRow$.emit(event.data);
+  }
+
+  onRowUnselect(): void {
+    this.procesosService.selectRow$.emit(null);
   }
 
 }
