c124eb874eb3c7f8945d3acb7a80f6f984fc0ff1
Alejandro Ramirez <josealejandroramz@gmail.com>
2022-05-25 13:05:02 -0400

    Feat agregada estructura de componentes, servicios e interfaces a empresa

--- src/app/modules/configuration/configuration-routing.module.ts
+++ src/app/modules/configuration/configuration-routing.module.ts
@@ -13,55 +13,68 @@ const routes: Routes = [
         path: '', component: ConfigHomeComponent
       },
       {
-        path: 'niveles-educativos',
-        // LazyLoad
-        loadChildren: () => import('./sub-modules/nomina/niveles-educativos/niveles-educactivos.module').then( m => m.NivelesEducactivosModule),
+        path: 'basica', children: [
+          {
+            path: 'niveles-educativos',
+            // LazyLoad
+            loadChildren: () => import('./sub-modules/nomina/niveles-educativos/niveles-educactivos.module').then( m => m.NivelesEducactivosModule),
+          },
+          {
+            path: 'profesiones',
+            // LazyLoad
+            loadChildren: () => import('./sub-modules/nomina/profesiones/profesiones.module').then( m => m.ProfesionesModule),
+          },
+          {
+            path: 'formas-pago',
+            // LazyLoad
+            loadChildren: () => import('./sub-modules/nomina/formas-pago/formas-pago.module').then( m => m.FormasPagoModule),
+          },
+          {
+            path: 'motivos-finiquito',
+            // LazyLoad
+            loadChildren: () => import('./sub-modules/nomina/motivos-finiquito/motivos-finiquito.module').then( m => m.MotivosFiniquitoModule),
+          },
+          {
+            path: 'procesos',
+            // LazyLoad
+            loadChildren: () => import('./sub-modules/nomina/procesos/procesos.module').then( m => m.ProcesosModule),
+          },
+          {
+            path: 'categories',
+            // LazyLoad
+            loadChildren: () => import('./sub-modules/nomina/categories/categories.module').then( m => m.CategoriesModule),
+          },
+          {
+            path: 'centros-medicos',
+            // LazyLoad
+            loadChildren: () => import('./sub-modules/nomina/centros-medicos/centros-medicos.module').then( m => m.CentrosMedicosModule),
+          },
+          {
+            path: 'clasificacion-oficial',
+            // LazyLoad
+            loadChildren: () => import('./sub-modules/nomina/clasificacion-oficial/clasificacion-oficial.module').then( m => m.ClasificacionOficialModule),
+          },
+          {
+            path: 'valor-oficial',
+            // LazyLoad
+            loadChildren: () => import('./sub-modules/nomina/valores-oficiales/valores-oficiales.module').then( m => m.ValoresOficialesModule),
+          },
+          {
+            path: 'sindicatos',
+            // LazyLoad
+            loadChildren: () => import('./sub-modules/nomina/sindicatos/sindicatos.module').then( m => m.SindicatosModule),
+          },
+        ]
       },
       {
-        path: 'profesiones',
-        // LazyLoad
-        loadChildren: () => import('./sub-modules/nomina/profesiones/profesiones.module').then( m => m.ProfesionesModule),
-      },
-      {
-        path: 'formas-pago',
-        // LazyLoad
-        loadChildren: () => import('./sub-modules/nomina/formas-pago/formas-pago.module').then( m => m.FormasPagoModule),
-      },
-      {
-        path: 'motivos-finiquito',
-        // LazyLoad
-        loadChildren: () => import('./sub-modules/nomina/motivos-finiquito/motivos-finiquito.module').then( m => m.MotivosFiniquitoModule),
-      },
-      {
-        path: 'procesos',
-        // LazyLoad
-        loadChildren: () => import('./sub-modules/nomina/procesos/procesos.module').then( m => m.ProcesosModule),
-      },
-      {
-        path: 'categories',
-        // LazyLoad
-        loadChildren: () => import('./sub-modules/nomina/categories/categories.module').then( m => m.CategoriesModule),
-      },
-      {
-        path: 'centros-medicos',
-        // LazyLoad
-        loadChildren: () => import('./sub-modules/nomina/centros-medicos/centros-medicos.module').then( m => m.CentrosMedicosModule),
-      },
-      {
-        path: 'clasificacion-oficial',
-        // LazyLoad
-        loadChildren: () => import('./sub-modules/nomina/clasificacion-oficial/clasificacion-oficial.module').then( m => m.ClasificacionOficialModule),
-      },
-      {
-        path: 'valor-oficial',
-        // LazyLoad
-        loadChildren: () => import('./sub-modules/nomina/valores-oficiales/valores-oficiales.module').then( m => m.ValoresOficialesModule),
-      },
-      {
-        path: 'sindicatos',
-        // LazyLoad
-        loadChildren: () => import('./sub-modules/nomina/sindicatos/sindicatos.module').then( m => m.SindicatosModule),
-      },
+        path: 'empresa', children: [
+          {
+            path: 'empresa',
+            // LazyLoad
+            loadChildren: () => import('./sub-modules/nomina/empresa/empresa/empresa.module').then( m => m.EmpresaModule),
+          }
+        ]
+      },  
       {
         path: '**', redirectTo: ''
       }
