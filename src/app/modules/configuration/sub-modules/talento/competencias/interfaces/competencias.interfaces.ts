8657577e1eba7d76d57ff9607b83fe8d52e10d8e
Jose Alejandro Ramirez <josealejandroramz@gmail.com>
2022-07-18 14:27:38 -0400

    Fix eliminada interfaces duplicada en Competencias

--- src/app/modules/configuration/sub-modules/talento/competencias/interfaces/competencias.interfaces.ts
+++ src/app/modules/configuration/sub-modules/talento/competencias/interfaces/competencias.interfaces.ts
@@ -2,13 +2,8 @@ export interface Competencias {
     id     :  number;
     nombre :  string;
     descrip:  string;
-    tipo   :  Tipo;
+    tipo   :  TiposCompetencias;
     links? :  Link[];
-}
-
-export interface Tipo {
-    id    : string;
-    nombre: string;
 }
 
 export interface Link {
