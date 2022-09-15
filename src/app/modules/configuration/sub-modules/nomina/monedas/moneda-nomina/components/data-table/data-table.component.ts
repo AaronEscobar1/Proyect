2737da9cfa39f04d588b1f54d9a3a10252878bf0
Jose Alejandro Ramirez <josealejandroramz@gmail.com>
2022-09-15 15:18:57 -0400

    Feat agregada estructura, servicios e interfaces de moneda a nomina

---
+++ src/app/modules/configuration/sub-modules/nomina/monedas/moneda-nomina/components/data-table/data-table.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { TableHead } from 'src/app/shared/interfaces/tableHead.interfaces';
+import { MonedaNomina } from '../../interfaces/moneda-nomina.interfaces';
+import { CompanyNominaService } from '../../../../empresa/shared-empresa/services/company-nomina.service';
+
+@Component({
+  selector: 'app-data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.scss']
+})
+export class DataTableComponent implements OnInit {
+
+  // Objeto para mostrar todos las monedas nominas por empresa y tipo nomina
+  @Input() monedasNominas: MonedaNomina[] = [];
+
+  // Table
+  columns: TableHead[] = [];
+
+  constructor(private companyNominaService: CompanyNominaService) { }
+
+  ngOnInit(): void {
+    this.columns = [
+      { field: 'fvigencia',             header: 'Vigencia'       },
+      { field: 'monedaid',              header: 'Tipo de moneda' },
+      { field: 'cfMonedaTipoTb.nombre', header: 'por nombre'     },
+      { field: 'comentario',            header: 'Comentario'     }
+    ];
+  }
+
+  onRowSelect(event: any): void {
+    this.companyNominaService.selectRowThirdTable$.emit(event.data);
+  }
+
+  onRowUnselect(): void {
+    this.companyNominaService.selectRowThirdTable$.emit(null);
+  }
+
+}
--- src/app/modules/configuration/sub-modules/nomina/monedas/moneda-nomina/pages/tipo-nomina/tipo-nomina.component.ts
+++ src/app/modules/configuration/sub-modules/nomina/monedas/moneda-nomina/pages/tipo-nomina/tipo-nomina.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { MessageService, ConfirmationService } from 'primeng/api';
-import { CompanyNominaService } from '../../../../empresa/shared-empresa/services/company-nomina.service';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { Company } from '../../../../empresa/shared-empresa/interfaces/empresa.interfaces';
 import { TipoNomina } from '../../../../empresa/shared-empresa/interfaces/nominas.interfaces';
-import { spinnerLight } from 'src/app/shared/components/spinner/spinner.interfaces';
 import { Subscription } from 'rxjs';
+import { CompanyNominaService } from '../../../../empresa/shared-empresa/services/company-nomina.service';
+import { MonedaNominaService } from '../../services/moneda-nomina.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { SelectRowService } from 'src/app/shared/services/select-row/select-row.service';
+import { spinnerLight } from 'src/app/shared/components/spinner/spinner.interfaces';
 
 @Component({
   selector: 'app-tipo-nomina',
@@ -15,19 +16,23 @@ import { SelectRowService } from 'src/app/shared/services/select-row/select-row.
 })
 export class TipoNominaComponent implements OnInit {
 
-  // Fila seleccionada para filtrar 
+  // Fila seleccionada para filtrar monedas nóminas por empresa
   @Input() empresaRow!: Company;
 
-  // Variable para obtener los tipos de nomina por empresa
+  // Variable para obtener las nomina por empresa
   tiposNominas: TipoNomina[] = [];
 
-  // Emisión de eventos (vaciar data)
-  @Output() onClearData  = new EventEmitter();
+  // Variable seleccionada de nomina
+  tipoNominaRow!: TipoNomina | null;
+
+  // Emisión de evento (cargar data monedas nóminas)
+  @Output() onGetDataGrupo = new EventEmitter();
 
   // Variable para manejar la suscripción
   subscriber!: Subscription;
 
   constructor(private companyNominaService: CompanyNominaService,
+              private monedaNominaService: MonedaNominaService,
               private spinner: NgxSpinnerService,
               private messageService: MessageService,
               private selectRowServices: SelectRowService) { }
@@ -35,25 +40,25 @@ export class TipoNominaComponent implements OnInit {
   ngOnInit(): void {
     // Se suscribe a los cambios que ocurran al cambiar de row en el datatable tipo nomina
     this.subscriber = this.selectRowServices.selectRowAlterno$.subscribe( (row: TipoNomina) => {
-      this.onClearData.emit();
+      this.tipoNominaRow = row;
+      this.loadMonedasNominas();
     });
   }
-
 
   ngOnChanges(): void {
     // Realizar peticion al backend al seleccionar una empresa
     if ( this.empresaRow && this.empresaRow.id) {
       this.loadNominas(this.empresaRow.id);
     } 
-    // Vaciar los tipos de nominas cuando la empresa se deselecciona
+    // Vaciar los tipos de nomninas cuando la empresa se deselecciona
     else if ( this.empresaRow == null ) {
       this.tiposNominas = []; 
     }
   }
 
-  loadNominas(idEmpresa: string): void {
+  loadNominas(id: string): void {
     this.spinner.show(undefined, spinnerLight);
-    this.companyNominaService.getAllNominasByEmpresa(idEmpresa)
+    this.companyNominaService.getAllNominasByEmpresa(id)
       .subscribe({
         next: (res: TipoNomina[]) => {
           this.tiposNominas = res;
@@ -65,8 +70,30 @@ export class TipoNominaComponent implements OnInit {
         }
       });
   }
-  
-  /** Destrucción del observable */
+
+  /**
+   * Obtener monedas nóminas relacionadas con una empresa y un tipo de nomina
+   */
+  loadMonedasNominas(): void {
+    console.log('load monedas');
+    if ( !this.tipoNominaRow ) {
+      return;
+    }
+    // this.spinner.show(undefined, spinnerLight);
+    // this.puntajeEvaluacionService.getAllPuntajesByEmpresaNomina(this.empresaRow.id, this.tipoNominaRow.tipnom)
+    //   .subscribe({
+    //     next: (res: PuntajeEvaluacion[]) => {
+    //       this.onGetDataGrupo.emit(res);
+    //       this.spinner.hide();
+    //     },
+    //     error: (err) => {
+    //       this.spinner.hide();
+    //       this.messageService.add({severity: 'warn', summary: 'Error', detail: 'No se pudo obtener conexión con el servidor.', life: 3000});
+    //     }
+    //   });
+  }
+
+  /** Destrucción del observable*/
   ngOnDestroy(): void {
     this.subscriber.unsubscribe();
   }
