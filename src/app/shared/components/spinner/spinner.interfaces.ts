3bdf4e26525860d6b211a9b6714ee9d13b1b6b47
Jose Ramirez <josealejandroramz@gmail.com>
2022-10-07 18:59:41 -0400

    Fix cambiado color del background y tamaño del spinner

--- src/app/shared/components/spinner/spinner.interfaces.ts
+++ src/app/shared/components/spinner/spinner.interfaces.ts
@@ -1,8 +1,8 @@
 import { Spinner } from "ngx-spinner";
 
 export const spinnerLight: Spinner = {
-    bdColor: 'rgba(51,51,51, 0)',
-    size: "medium",
-    color: "#62C3B9",
-    type: "ball-circus"
+    bdColor: 'rgba(51,51,51, 0.08)',
+    size:    'large',
+    color:   '#316660',
+    type:    'ball-circus'
 }
