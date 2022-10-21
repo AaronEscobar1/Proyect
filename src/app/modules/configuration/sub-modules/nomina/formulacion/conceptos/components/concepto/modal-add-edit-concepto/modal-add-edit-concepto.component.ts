aec4418396b4ae5ce5452239afc925e76466b046
Jose Alejandro Ramirez <josealejandroramz@gmail.com>
2022-10-20 23:45:42 -0400

    Feat agregado estructura, pages, componentes, interfaces y servicios en conceptos

---
+++ src/app/modules/configuration/sub-modules/nomina/formulacion/conceptos/components/concepto/modal-add-edit-concepto/modal-add-edit-concepto.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-add-edit-concepto',
+  templateUrl: './modal-add-edit-concepto.component.html'
+})
+export class ModalAddEditConceptoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/configuration/sub-modules/nomina/formulacion/conceptos/conceptos.module.ts
+++ src/app/modules/configuration/sub-modules/nomina/formulacion/conceptos/conceptos.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ConceptosRoutingModule } from './conceptos-routing.module';
 
-// Components
+// Components Concepto
+import { ConceptosComponent } from './pages/conceptos/conceptos.component';
 import { ConceptosHomeComponent } from './pages/conceptos-home/conceptos-home.component';
+import { DataTableConceptoComponent } from './components/concepto/data-table-concepto/data-table-concepto.component';
+import { ModalAddEditConceptoComponent } from './components/concepto/modal-add-edit-concepto/modal-add-edit-concepto.component';
+import { ModalPrintConceptoComponent } from './components/concepto/modal-print-concepto/modal-print-concepto.component';
+import { TipoNominaComponent } from './pages/tipo-nomina/tipo-nomina.component';
 
 @NgModule({
   declarations: [
-    ConceptosHomeComponent
+    ConceptosComponent,
+    ConceptosHomeComponent,
+    DataTableConceptoComponent,
+    ModalAddEditConceptoComponent,
+    ModalPrintConceptoComponent,
+    TipoNominaComponent
   ],
   imports: [
     SharedModule,
